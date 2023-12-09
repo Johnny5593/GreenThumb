a62bb71e62b624432ea11062d630104668c8e218
jon simouni <jon.simouni@gmail.com>
2023-12-10 00:02:48 +0100

    Adds Entities class and DbContext to start Code first

---
+++ DbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GreenThumb
+{
+    public class YourDbContext : DbContext
+    {
+        public DbSet<Plant> Plants { get; set; }
+        public DbSet<Instruction> Instructions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Plant>()
+                .HasMany(p => p.FollowingInstructions)
+                .WithOne(i => i.Plant)
+                .HasForeignKey(i => i.PlantId);
+        }
+    }
+}
---
+++ Entities.cs
@@ -0,0 +1,24 @@
+﻿namespace GreenThumb
+{
+    public class Plant
+    {
+        public int PlantId { get; set; }
+        public string PlantName { get; set; }
+        public ICollection<Instruction> FollowingInstructions { get; set; }
+    }
+
+    public class Instruction
+    {
+        public int InstructionId { get; set; }
+        public string InstructionText { get; set; }
+        public int PlantId { get; set; }
+        public Plant Plant { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
